73957e33e29d4c15a02b2e186e77736d85b65490
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2023-07-07 21:39:05 -0300

    Update Jwt.js

--- Jwt.js
+++ Jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-dotenv.config({path:"backend/config.env"})
+dotenv.config({path:"config.env"})
 
 const generatetoken = (user_id) => {
     const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRY });
